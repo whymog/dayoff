ec5b5bfb11a9d9bef9c3fff0917efb4ee05ef58e
Ben Morgan <benjaminmorgan89@gmail.com>
2018-12-27 13:36:55 -0800

    Added game state manager script

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour
+{
+    public bool IsInteractMode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsInteractMode = false;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
