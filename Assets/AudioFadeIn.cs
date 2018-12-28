c80462927b88b77354e671cbff4f57205a621a40
Ben Morgan <benjaminmorgan89@gmail.com>
2018-12-28 15:09:26 -0800

    Added script to fade in audio for room scene.

---
+++ Assets/AudioFadeIn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioFadeIn : MonoBehaviour
+{
+        [SerializeField]
+    private int m_FadeInTime = 10;
+    private AudioSource m_AudioSource;
+
+    void Awake()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (m_AudioSource.volume < 1)
+        {
+            m_AudioSource.volume = m_AudioSource.volume + (Time.deltaTime / (m_FadeInTime + 1));
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
+
